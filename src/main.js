7fb4a03c62dbb9d3f7744f33bc44d1308538b190
Shawn_Liang <102449877+shawnworld02@users.noreply.github.com>
2022-07-08 13:31:45 +0800

    添加验证表单

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { Button } from "element-ui";
+import { Button, Form, FormItem, Input, Row, Col } from "element-ui";
 
 Vue.config.productionTip = false;
 Vue.use(Button);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(Row);
+Vue.use(Col);
 new Vue({
   router,
   store,
--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -11,6 +11,60 @@
           {{ item.text }}
         </li>
       </ul>
+      <!--表单验证-->
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        class="login-form"
+        size="medium"
+      >
+        <el-form-item prop="username" class="item-form">
+          <label>用户名</label>
+
+          <el-input
+            type="text"
+            v-model="ruleForm.username"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password" class="item-form">
+          <label>密码</label>
+          <el-input
+            type="password"
+            v-model="ruleForm.password"
+            autocomplete="off"
+            minlength="6"
+            maxlength="20"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="code" class="item-form">
+          <label>验证码</label>
+          <el-row :gutter="10">
+            <el-col :span="15"
+              ><el-input
+                v-model.number="ruleForm.code"
+                minlength="6"
+                maxlength="6"
+              ></el-input
+            ></el-col>
+            <el-col :span="9"
+              ><el-button type="success" class="block"
+                >获取验证码</el-button
+              ></el-col
+            >
+          </el-row>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            type="danger"
+            @click="submitForm('ruleForm')"
+            class="login-btn block"
+            >登录</el-button
+          >
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -19,14 +73,61 @@
 export default {
   name: "LoginPage",
   data() {
+    //验证用户名
+    const validateUsername = (rule, value, callback) => {
+      //邮箱验证正则
+      let reg =
+        /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
+      if (value === "") {
+        callback(new Error("请输入用户名"));
+      } else if (!reg.test(value)) {
+        callback(new Error("用户名格式有误"));
+      } else {
+        callback();
+      }
+    };
+    //验证密码
+    const validatePassword = (rule, value, callback) => {
+      let reg = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/;
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else if (!reg.test(value)) {
+        callback(new Error("密码格式有误，应为6-20位数字或字母"));
+      } else {
+        callback();
+      }
+    };
+    //验证验证码
+    const checkCode = (rule, value, callback) => {
+      let reg = /^[a-z0-9]{6}$/;
+      if (value === "") {
+        return callback(new Error("请输入验证码"));
+      } else if (!reg.test(value)) {
+        return callback("验证码格式有误");
+      } else {
+        callback();
+      }
+    };
     return {
       menuTab: [
         { text: "登录", id: 1 },
         { text: "注册", id: 2 },
       ],
       currentIndexOfItem: 0,
+      ruleForm: {
+        username: "",
+        password: "",
+        code: "",
+      },
+      rules: {
+        //当失去焦点时，就会触发验证方法
+        username: [{ validator: validateUsername, trigger: "blur" }],
+        password: [{ validator: validatePassword, trigger: "blur" }],
+        code: [{ validator: checkCode, trigger: "blur" }],
+      },
     };
   },
+
   created() {},
   mounted() {
     //挂载完成后自动执行
@@ -34,6 +135,16 @@ export default {
   methods: {
     handleClick(index) {
       this.currentIndexOfItem = index;
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
   },
 };
@@ -61,6 +172,25 @@ export default {
   }
   .current {
     background-color: rgba(0, 0, 0, 0.1);
+  }
+}
+.login-form {
+  margin-top: 29px;
+  label {
+    display: block;
+    margin-bottom: 3px;
+    font-size: 14px;
+    color: #fff;
+  }
+  .item-form {
+    margin-bottom: 30px;
+  }
+  .block {
+    display: block;
+    width: 100%;
+  }
+  .login-btn {
+    margin-top: 19px;
   }
 }
 </style>
