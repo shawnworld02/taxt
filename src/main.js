dcdb062d94a2fc90155abab0dcc9c43ae2e50387
Shawn_Liang <102449877+shawnworld02@users.noreply.github.com>
2022-07-08 23:16:43 +0800

    vue3语法

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueCompositionApi from "@vue/composition-api";
 import { Button, Form, FormItem, Input, Row, Col } from "element-ui";
 
 Vue.config.productionTip = false;
+Vue.use(VueCompositionApi);
 Vue.use(Button);
 Vue.use(Form);
 Vue.use(FormItem);
